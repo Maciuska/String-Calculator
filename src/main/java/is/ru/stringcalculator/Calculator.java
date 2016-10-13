8837cda879d5e81f0b4dbb72d3c30a01a281a404
Maciuska <Maciuska14@gmail.com>
2016-10-13 18:25:59 +0000

    testTwoNumbers seems to be working well so add returns 3 using text.split

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,11 +6,14 @@ public class Calculator {
 		if(text.equals("")){
 			return 0;
 		}
+		else if(text.contains(",")){
+			String [] numbers = text.split(",");
+			return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+			//return sum(splitNumbers(text));
+		}
 		else
 			return 1;
-		/*else if(text.contains(",")){
-			return sum(splitNumbers(text));
-		}*/
+
 
 	}
 	/*
