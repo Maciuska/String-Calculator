206d3f5090f1eace0419a2238de37e80c9fd5db7
Maciuska <Maciuska14@gmail.com>
2016-10-13 18:20:12 +0000

    testOneNumber added and seems to be working when add() returns 1

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,16 +3,17 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text){
-		//if(text.equals("")){
+		if(text.equals("")){
 			return 0;
 		}
+		else
+			return 1;
 		/*else if(text.contains(",")){
 			return sum(splitNumbers(text));
-		}
-		else
-			return 1;*/
-	/*}
+		}*/
 
+	}
+	/*
 	private static int toInt(String number){
 		return Integer.parseInt(number);
 	}
