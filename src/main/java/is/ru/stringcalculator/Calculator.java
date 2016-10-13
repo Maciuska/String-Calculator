7602709cba568278d72d9b067dc27dff0ba5d4fe
Maciuska <Maciuska14@gmail.com>
2016-10-13 18:11:12 +0000

    Baby steps: checking if testEmptyString works when add returns 0

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -4,7 +4,7 @@ public class Calculator {
 
 	public static int add(String text){
 		//if(text.equals("")){
-			return 3;
+			return 0;
 		}
 		/*else if(text.contains(",")){
 			return sum(splitNumbers(text));
